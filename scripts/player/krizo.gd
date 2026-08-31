d3adf58d33995e5909b6d7cc50f0fe8a07c2ca61
André <almeida.andref@gmail.com>
2026-08-31 14:53:56 -0700

    Add mouse drag steering and head bump behavior

--- scripts/player/krizo.gd
+++ scripts/player/krizo.gd
@@ -6,6 +6,8 @@ signal fuel_changed(ratio: float)
 signal crashed
 signal coin_collected(total_this_run: int)
 
+const BASE_VISUAL_SCALE := Vector2(1.25, 1.25)
+
 var gravity: float = GameBalance.BASE_GRAVITY
 var thrust: float = GameBalance.BASE_THRUST
 var horizontal_speed: float = GameBalance.BASE_HORIZONTAL_SPEED
@@ -13,7 +15,7 @@ var fuel_capacity: float = GameBalance.BASE_FUEL_CAPACITY
 var fuel_burn_per_second: float = GameBalance.BASE_BURN_RATE
 var fuel_recharge_per_second: float = GameBalance.BASE_RECHARGE_RATE
 var max_fall_speed: float = 720.0
-var horizontal_acceleration: float = 1150.0
+var horizontal_acceleration: float = 1500.0
 
 var fuel: float = 0.0
 var start_y: float = 0.0
@@ -21,6 +23,8 @@ var alive: bool = true
 var run_coins: int = 0
 var touch_active: bool = false
 var touch_x: float = 360.0
+var mouse_active: bool = false
+var mouse_x: float = 360.0
 
 @onready var body: Polygon2D = $Visual/Body
 @onready var flame: Polygon2D = $Visual/Flame
@@ -30,6 +34,7 @@ func _ready() -> void:
 	start_y = global_position.y
 	_apply_upgrades()
 	fuel = fuel_capacity
+	visual.scale = BASE_VISUAL_SCALE
 	fuel_changed.emit(1.0)
 
 func _apply_upgrades() -> void:
@@ -47,9 +52,11 @@ func _physics_process(delta: float) -> void:
 		return
 
 	var horizontal: float = Input.get_axis("move_left", "move_right")
-	if touch_active:
-		var horizontal_error: float = touch_x - global_position.x
-		horizontal = clampf(horizontal_error / 130.0, -1.0, 1.0)
+	var pointer_active: bool = touch_active or mouse_active
+	if pointer_active:
+		var target_x: float = touch_x if touch_active else mouse_x
+		var horizontal_error: float = target_x - global_position.x
+		horizontal = clampf(horizontal_error / 105.0, -1.0, 1.0)
 	velocity.x = move_toward(velocity.x, horizontal * horizontal_speed, horizontal_acceleration * delta)
 
 	var boosting: bool = (Input.is_action_pressed("jetpack") or touch_active) and fuel > 0.0
@@ -77,6 +84,14 @@ func _unhandled_input(event: InputEvent) -> void:
 		var drag_event: InputEventScreenDrag = event as InputEventScreenDrag
 		touch_active = true
 		touch_x = drag_event.position.x
+	elif event is InputEventMouseButton:
+		var mouse_button: InputEventMouseButton = event as InputEventMouseButton
+		if mouse_button.button_index == MOUSE_BUTTON_LEFT:
+			mouse_active = mouse_button.pressed
+			mouse_x = mouse_button.position.x
+	elif event is InputEventMouseMotion and mouse_active:
+		var mouse_motion: InputEventMouseMotion = event as InputEventMouseMotion
+		mouse_x = mouse_motion.position.x
 
 func _update_visuals(boosting: bool, delta: float) -> void:
 	flame.visible = boosting
@@ -84,11 +99,19 @@ func _update_visuals(boosting: bool, delta: float) -> void:
 		flame.scale.y = 0.85 + randf() * 0.35
 	var lean: float = clampf(velocity.x / maxf(horizontal_speed, 1.0), -1.0, 1.0) * 0.22
 	visual.rotation = lerpf(visual.rotation, lean, 8.0 * delta)
-	var target_scale: Vector2 = Vector2(0.97, 1.05) if boosting else Vector2.ONE
+	var squash: Vector2 = Vector2(0.97, 1.05) if boosting else Vector2.ONE
+	var target_scale: Vector2 = BASE_VISUAL_SCALE * squash
 	visual.scale = visual.scale.lerp(target_scale, 10.0 * delta)
 
 func current_altitude() -> int:
 	return maxi(0, int((start_y - global_position.y) / GameBalance.PIXELS_PER_METER))
+
+func bump_head() -> void:
+	if not alive:
+		return
+	velocity.y = maxf(velocity.y, 115.0)
+	fuel = maxf(0.0, fuel - 0.12)
+	fuel_changed.emit(fuel / fuel_capacity)
 
 func crash() -> void:
 	if not alive:
