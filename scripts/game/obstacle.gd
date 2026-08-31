462224a4c97eb3909b41609e774b5d75bfe89515
André <almeida.andref@gmail.com>
2026-08-31 15:03:07 -0700

    Make every obstacle impact survivable

--- scripts/game/obstacle.gd
+++ scripts/game/obstacle.gd
@@ -4,11 +4,6 @@ func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body: Node) -> void:
-	if body is not KrizoPlayer:
-		return
-	var krizo: KrizoPlayer = body as KrizoPlayer
-	var hit_from_below: bool = krizo.velocity.y < 0.0 and krizo.global_position.y > global_position.y + 8.0
-	if hit_from_below:
-		krizo.bump_head()
-	else:
-		krizo.crash()
+	if body is KrizoPlayer:
+		var krizo: KrizoPlayer = body as KrizoPlayer
+		krizo.hit_obstacle(global_position)
